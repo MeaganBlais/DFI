c38d39bb5fc837ba01a70000e03ed07cd02ac344
Meagan Blais <meagan.blais@gmail.com>
2018-09-10 23:46:29 -0400

    adding responsive navBar

--- resources/js/script.js
+++ resources/js/script.js
@@ -10,4 +10,20 @@ $(document).ready(function() {
     offset: '25%'
   });
   
+  
+  /* --- Mobile nav ---*/
+  $('.js--nav-icon').click(function() {
+    const nav = $('.js--main-nav');
+    const icon = $('.js--nav-icon i');
+    
+    nav.slideToggle(200);
+    
+    if (icon.hasClass('fas fa-bars')) {
+      icon.addClass('fas fa-times');
+      icon.removeClass('fas fa-bars');
+    } else {
+      icon.addClass('fas fa-bars');
+      icon.removeClass('fas fa-times');
+    }
+  });
 });
